e4486e52a510379b6fc4fa25f41d08ce7cb7f1bf
RinaKorca <rk48713@ubt-uni.net>
2024-08-07 13:49:37 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const p1Button = document.querySelector("#p1Button");
+const p2Button = document.querySelector("#p2Button");
+const p2Display = document.querySelector("#p2Display");
+
+let p1Score = 0;
+
+p1Button.addEventListener("click", function () {
+  p1Score += 1;
+});
